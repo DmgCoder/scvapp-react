77ecdf58a00d05e7e7af8810a99188a7207cb052
Urban Krepel <urban.kreppel@gmail.com>
2022-06-16 18:26:20 +0200

    Popravil gumb v stranski vrstici

--- src/components/sidebarLink.js
+++ src/components/sidebarLink.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class SidebarLink extends React.Component{
-    render(){
-        return(
-            <li className={`sideLink ${this.props.className} ${this.props.pathname === this.props.href && "active"}`}>
-                <Link to={this.props.href||""}>
-                    <div className="link">
-                        <div className="icon">{this.props.children}</div>
-                        {
-                            this.props.size >= 300 ? <span className="item">{this.props.name}</span> : ""
-                        }
-                    </div>
-                    </Link>
-                </li>
-        )
-    }
+class SidebarLink extends React.Component {
+  render() {
+    return (
+      <li
+        className={`sideLink ${this.props.className} ${
+          detectIfActive(this.props.pathname, this.props.href) && "active"
+        }`}
+      >
+        <Link to={this.props.href || ""}>
+          <div className="link">
+            <div className="icon">{this.props.children}</div>
+            {this.props.size >= 300 ? (
+              <span className="item">{this.props.name}</span>
+            ) : (
+              ""
+            )}
+          </div>
+        </Link>
+      </li>
+    );
+  }
 }
 
-export default SidebarLink
+function detectIfActive(pathname, href) {
+  if (
+    (pathname === "/" && href === "/") ||
+    (pathname === "/domov" && href === "/")
+  ) {
+    return true;
+  } else if (href !== "/") {
+    return pathname.startsWith(href);
+  }
+  return false;
+}
+
+export default SidebarLink;
