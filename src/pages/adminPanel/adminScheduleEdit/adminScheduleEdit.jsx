faf1486dc690151c7bbcfdc9962833a6a6dbcdb2
Urban Krepel <urban.kreppel@gmail.com>
2022-10-15 14:23:32 +0200

    Added school select

--- src/pages/adminPanel/adminScheduleEdit/adminScheduleEdit.jsx
+++ src/pages/adminPanel/adminScheduleEdit/adminScheduleEdit.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectTheme } from "../../../features/theme/themeSlice";
+import SelectSchoolDropdown from "../components/selectSchoolDropdown/selectSchoolDropdown";
 
 import "./adminScheduleEdit.css";
 
 const AdminScheduleEdit = () => {
   const theme = useSelector(selectTheme);
-  return <div className={`admin-schedule-edit ${theme}`}>adminScheduleEdit</div>;
+  const [selectedSchool, setSelectedSchool] = React.useState("ERS");
+
+  const schools = {
+    GIM: "GIM",
+    ERS: "ERŠ",
+    SSD: "ŠSD",
+    SSGO: "ŠSGO",
+  };
+
+  const selectSchool = (e) => {
+    setSelectedSchool(e.target.id);
+  };
+
+  return (
+    <div className={`admin-schedule-edit ${theme}`}>
+      <div className="admin-schedule-edit-select-school">
+        <p>Izbrana šola</p>
+        <SelectSchoolDropdown
+          schools={schools}
+          selectedSchool={selectedSchool}
+          selectSchool={selectSchool}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AdminScheduleEdit;
---
+++ src/pages/adminPanel/components/selectSchoolDropdown/selectSchoolDropdown.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectTheme } from "../../../../features/theme/themeSlice";
+
+import "./selectSchoolDropdown.css";
+
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const SelectSchoolDropdown = ({ schools, selectedSchool, selectSchool }) => {
+  const theme = useSelector(selectTheme);
+  const [showDropdown, setShowDropdown] = React.useState(false);
+
+  const clickSelectSchool = (e) => {
+    selectSchool(e);
+    setShowDropdown(false);
+  };
+
+  return (
+    <div
+      className={`admin-select-school-dropdown ${theme} ${
+        showDropdown && "admin-select-school-dropdown-show"
+      }`}
+    >
+      <p className="admin-select-school-dropdown-text">
+        {schools[selectedSchool] ?? "---"}
+      </p>
+      <button
+        className="admin-selected-school-dropdown-btn"
+        style={
+          showDropdown
+            ? { transform: "rotate(180deg)" }
+            : { transform: "rotate(0deg)" }
+        }
+        onClick={() => setShowDropdown(!showDropdown)}
+      >
+        <KeyboardArrowDownIcon />
+      </button>
+      <div className="admin-select-school-dropdown-main">
+        {Object.keys(schools)
+          .filter((school) => school !== selectedSchool)
+          .map((school, i) => (
+            <p
+              key={i}
+              id={school}
+              className="admin-select-school-dropdown-main-option"
+              onClick={clickSelectSchool}
+            >
+              {schools[school]}
+            </p>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SelectSchoolDropdown;
