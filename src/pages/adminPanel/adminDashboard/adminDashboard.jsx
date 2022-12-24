6d9dbe052a8839960f44f8545c700864de9d3bfc
Urban Krepel <urban.kreppel@gmail.com>
2022-12-24 16:25:55 +0100

    Added exit btn to admin dashboard

--- src/pages/adminPanel/adminDashboard/adminDashboard.jsx
+++ src/pages/adminPanel/adminDashboard/adminDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectTheme } from "../../../features/theme/themeSlice";
+import { Link } from "react-router-dom";
 
 import "./adminDashboard.css";
 
@@ -10,6 +11,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
 import SideMenuProfile from "../../../components/SideMenuProfile/sideMenuProfile";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 const AdminDashboard = () => {
   const theme = useSelector(selectTheme);
@@ -36,6 +38,10 @@ const AdminDashboard = () => {
         />
       </div>
       <div className="admin-dashboard-bottom">
+        <Link to="/" className="admin-dashboard-exit">
+          <CancelIcon />
+          <p>Izhod iz plošče</p>
+        </Link>
         <div className="admin-dashboard-bottom-line"></div>
         <SideMenuProfile />
       </div>
