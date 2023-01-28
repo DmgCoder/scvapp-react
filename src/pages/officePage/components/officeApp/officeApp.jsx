7e4e76d73858f4cb77b325ca2e7b363b8d1767bd
Urban Krepel <urban.kreppel@gmail.com>
2023-01-28 09:12:49 +0100

    Update office page

--- src/pages/officePage/components/officeApp/officeApp.jsx
+++ src/pages/officePage/components/officeApp/officeApp.jsx
@@ -4,12 +4,12 @@ import "./officeApp.css";
 
 import WordIcon from "../../../../assets/office_icons/Word.svg";
 
-const OfficeApp = ({ icon, appName }) => {
+const OfficeApp = ({ icon, appName, href }) => {
   return (
-    <div className="office-page-app">
+    <a className="office-page-app" href={href} target="_blank" rel="noreferrer">
       <img src={icon ?? WordIcon} alt={`${appName} Icon`} />
       <p>{appName ?? ""}</p>
-    </div>
+    </a>
   );
 };
 
--- src/pages/officePage/officePage.jsx
+++ src/pages/officePage/officePage.jsx
@@ -12,9 +12,11 @@ import TeamsIcon from "../../assets/office_icons/Teams.svg";
 import OneDriveIcon from "../../assets/office_icons/OneDrive.svg";
 import Office356Icon from "../../assets/office_icons/Office365.svg";
 import useSideMenu from "../../features/sideMenu/useSideMenu";
+import { useUser } from "../../features/user/userHook";
 
 const OfficePage = () => {
   const { sideMenuMini } = useSideMenu();
+  const { user } = useUser();
   return (
     <div
       className="office-page"
@@ -23,14 +25,48 @@ const OfficePage = () => {
         left: `${sideMenuMini ? "60px" : "250px"}`,
       }}
     >
-      <OfficeApp icon={WordIcon} appName="Word" />
-      <OfficeApp icon={ExcelIcon} appName="Excel" />
-      <OfficeApp icon={PowerPointIcon} appName="PowerPoint" />
-      <OfficeApp icon={OutlookIcon} appName="Outlook" />
-      <OfficeApp icon={OneDriveIcon} appName="OneDrive" />
-      <OfficeApp icon={TeamsIcon} appName="Teams" />
-      <OfficeApp icon={OneNoteIcon} appName="OneNote" />
-      <OfficeApp icon={Office356Icon} appName="Office 365 portal" />
+      <OfficeApp
+        icon={WordIcon}
+        appName="Word"
+        href={`https://www.office.com/launch/word?auth=2&username=${user.mail}&login_hint=${user.mail}`}
+      />
+      <OfficeApp
+        icon={ExcelIcon}
+        appName="Excel"
+        href={`https://www.office.com/launch/excel?auth=2&username=${user.mail}&login_hint=${user.mail}`}
+      />
+      <OfficeApp
+        icon={PowerPointIcon}
+        appName="PowerPoint"
+        href={`https://www.office.com/launch/powerpoint?auth=2&username=${user.mail}&login_hint=${user.mail}`}
+      />
+      <OfficeApp
+        icon={OutlookIcon}
+        appName="Outlook"
+        href={`https://outlook.office.com/owa/?realm=scv.si&exsvurl=1&ll-cc=1060&modurl=0&login_hint=${user.mail}`}
+      />
+      <OfficeApp
+        icon={OneDriveIcon}
+        appName="OneDrive"
+        href={`https://scvsi-my.sharepoint.com/personal/${user.mail
+          .replaceAll(".", "_")
+          .replaceAll("@", "_")}/_layouts/15/onedrive.aspx?`}
+      />
+      <OfficeApp
+        icon={TeamsIcon}
+        appName="Teams"
+        href={`https://aka.ms/mstfw?login_hint_safe=${user.mail}`}
+      />
+      <OfficeApp
+        icon={OneNoteIcon}
+        appName="OneNote"
+        href={`https://www.office.com/launch/onenote?auth=2&username=${user.mail}&login_hint=${user.mail}`}
+      />
+      <OfficeApp
+        icon={Office356Icon}
+        appName="Office 365 portal"
+        href={`https://www.office.com/?auth=2&username=${user.mail}&login_hint=${user.mail}`}
+      />
     </div>
   );
 };
