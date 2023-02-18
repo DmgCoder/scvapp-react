b6e66cc943396c15b5507e1ff003c32222839bc4
Urban Krepel <urban.kreppel@gmail.com>
2023-02-18 17:55:18 +0100

    Update officeApp.jsx

--- src/pages/officePage/components/officeApp/officeApp.jsx
+++ src/pages/officePage/components/officeApp/officeApp.jsx
@@ -3,10 +3,22 @@ import React from "react";
 import "./officeApp.css";
 
 import WordIcon from "../../../../assets/office_icons/Word.svg";
+import { setOfficeAppsShown } from "../../../../features/apps/appsSlice";
 
 const OfficeApp = ({ icon, appName, href }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setOfficeAppsShown(false));
+  };
   return (
-    <a className="office-page-app" href={href} target="_blank" rel="noreferrer">
+    <a
+      className="office-page-app"
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      onClick={handleClick}
+    >
       <img src={icon ?? WordIcon} alt={`${appName} Icon`} />
       <p>{appName ?? ""}</p>
     </a>
