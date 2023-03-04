69f685054379b82d242deb70feda61980f8de626
Urban Krepel <urban.kreppel@gmail.com>
2023-03-04 20:47:24 +0100

    Add web meals

---
+++ src/pages/mealsPage/mealsWeb.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MealsWeb = () => {
+  return (
+    <iframe
+      style={{ width: "100%", height: "100%", border: "none" }}
+      src="https://malice.scv.si"
+    ></iframe>
+  );
+};
+
+export default MealsWeb;
